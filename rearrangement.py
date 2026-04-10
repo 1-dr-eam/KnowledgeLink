47a20b0b8b2163df71d683f1d75e8103f006b095
1dream <2567087865@qq.com>
2026-04-10 14:42:18 +0800

    调参

--- rearrangement.py
+++ rearrangement.py
@@ -18,7 +18,7 @@ class MmrDiversity:
         self.items = []
         self.lambda_param = 0.7
         self.selection_count = 50
-        self.window_size = 50
+        self.window_size = 30
         # 物品间的余弦相似度矩阵（基于内容）
         self.item_id_index = {} #在build_cosine_similarity_matrix填充，这里的index就是矩阵的index
         self.cosine_similarity_matrix = None
